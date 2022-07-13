c2ddda4624bc2cbeb79368790cd47b9efcd5b153
zuka <zuka@DESKTOP-01OQQMQ>
2022-07-13 21:29:58 +0400

    Add project files.

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,13 @@
+﻿static bool IsPalindrome(string text)
+{
+    for (int i = 0; i < text.Length / 2; ++i)
+    {
+        if (text[i] != text[text.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine(IsPalindrome("zukakuz"));
